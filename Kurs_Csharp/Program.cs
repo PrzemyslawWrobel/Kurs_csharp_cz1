7abb510310eefcc92ff826eec070a991c0847f46
PW <przemek.wrobel@poczta.fm>
2019-12-04 14:49:20 +0100

    Dodane metody

--- Kurs_Csharp/Program.cs
+++ Kurs_Csharp/Program.cs
@@ -1,20 +1,82 @@
 ﻿using System;
 
-namespace Kurs_Csharp
+namespace Hello
+
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
-           // string name = args[0];
-           // Console.WriteLine("Hello " + name);
+            ValuesTypes();
 
-            foreach (var item in args)
+            for (; ; )
             {
-                Console.WriteLine("Hello " + item);
+                Greeting();
+                Age();
+                Settings();
             }
 
-            //  fdsr
+        }
+
+        private static void ValuesTypes()
+        {
+            int minint = int.MinValue;
+            int maxint = int.MaxValue;
+            long minlong = long.MinValue;
+            long maxlong = long.MaxValue;
+
+            Console.WriteLine("minint: " + minint);
+            Console.WriteLine("maxint: " + maxint);
+            Console.WriteLine("minlong: " + minlong);
+            Console.WriteLine("maxlong: " + maxlong);
+        }
+
+        /// <summary>
+        /// Metoda resetuje ustawienia
+        /// </summary>
+        private static void Settings()
+        {
+            Console.ReadKey();
+            Console.Clear();
+            Console.ResetColor();
+        }
+        
+        /// <summary>
+        /// wypisujemy komunikat zależny od wieku
+        /// </summary>
+        private static void Age()
+        {
+            Console.Write("Wpisz wiek: ");
+            int age;
+            bool result = int.TryParse(Console.ReadLine(), out age);
+            Console.WriteLine(age);
+
+
+            if (result == false)
+            {
+                Console.WriteLine("Wiek musi być cyfrą np: 18");
+            }else if (age > 18)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Wypij Flaszkę");
+            }
+            else
+            {
+                Console.WriteLine("Pij Mleko");
+            }
+        }
+        
+        /// <summary>
+        /// Wypisuje w consoli imię użytkownika
+        /// </summary>
+        /// <returns></returns>
+        private static string Greeting()
+        {
+            Console.Write("Wpisz swoje imię: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+            return name;
         }
     }
 }
